d13cace896ddac14fbf3d7791d910ba14fcfc1e5
H-affy <abdullateefadeluola@gmail.com>
2023-08-10 05:27:47 -0700

    Calculation

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+def add(a, b):
+    """My addition function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    ARgs:
+    a: first integer
+    b: second integer
+
+    Returns:
+    The return value. a / b
+    """
+    return (a / b)
