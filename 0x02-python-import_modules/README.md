448ea00bc29aa87efbae9127a8f520b64ed4c3c9
H-affy <abdullateefadeluola@gmail.com>
2023-08-11 06:34:15 -0700

    Add

--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,1 +1,1 @@
-Python import modules
+Python importing modules
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -3,11 +3,10 @@ def add(a, b):
     """My addition function
 
     Args:
-
     a: first integer
     b: second integer
 
     Returns:
-    The return value.a + b
+    The return value. a + b
     """
     return (a + b)
