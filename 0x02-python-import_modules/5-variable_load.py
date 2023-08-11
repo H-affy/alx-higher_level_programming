922cc99de608501c2c54d4272ed4752849f02691
H-affy <abdullateefadeluola@gmail.com>
2023-08-11 14:19:40 -0700

    updated

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Import variable a from the variable_load_5.py file and prints its value"""
+    """Import variable a from the variable_load_5.py file
+    and prints its value"""
     from variable_load_5 import a
 
     print(a)
