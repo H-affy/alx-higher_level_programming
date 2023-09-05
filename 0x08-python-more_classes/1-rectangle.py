33f563c3996e829c6f4e7dff36bf9f788473deab
H-affy <abdullateefadeluola@gmail.com>
2023-09-04 17:46:51 -0700

    Rectangles

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/pyrhon3
+"""defines the rectangle class"""
+
+
+class Rectangle:
+    """represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """initializes a new rectangle.
+
+        Args:
+        width (int): The width of the new rectangle
+        height (int): the height of the new rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Set/get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
