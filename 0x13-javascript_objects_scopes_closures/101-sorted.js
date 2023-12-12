8ee0ec3e287f223e0c086f8fcff35f51da57a131
H-affy <abdullateefadeluola@gmail.com>
2023-12-12 05:16:22 -0800

    Fixing errors

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,10 +3,10 @@ const dict = require('./101-data').dict;
 const newDiction = {};
 
 Object.keys(dict).map(function (key, index) {
-	if (newDiction[dict[key]] === undefined) {
-		newDiction[dict[key]] = [];
-	}
-	newDiction[dict[key]].push(key);
+  if (newDiction[dict[key]] === undefined) {
+    newDiction[dict[key]] = [];
+  }
+  newDiction[dict[key]].push(key);
 });
 
 console.log(newDiction);
