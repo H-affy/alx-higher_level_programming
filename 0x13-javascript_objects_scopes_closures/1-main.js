b6221ef5986b9755e407fa2267b55f770d683c3d
H-affy <abdullateefadeluola@gmail.com>
2023-12-12 04:25:01 -0800

    fixing error

--- 0x13-javascript_objects_scopes_closures/1-main.js
+++ 0x13-javascript_objects_scopes_closures/1-main.js
@@ -11,7 +11,7 @@ console.log(r2);
 console.log(r2.width);
 console.log(r2.height);
 
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
+const r3 = new Rectangle(2);
+console.log(r3);
+console.log(r3.width);
+console.log(r3.height);
