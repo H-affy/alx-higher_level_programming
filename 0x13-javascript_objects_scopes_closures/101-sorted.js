0aca9b3de2bf499bc24f523b8f98c9fe57c259c1
H-affy <abdullateefadeluola@gmail.com>
2023-12-12 05:30:31 -0800

    Fixing errors

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const newDiction = {};
 
-Object.keys(dict).map(function (key, index) {
-  if (newDiction[dict[key]] === undefined) {
-    newDiction[dict[key]] = [];
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
   }
-  newDiction[dict[key]].push(key);
-});
-
-console.log(newDiction);
+  newDict[valsUniq[j]] = list;
+}
+console.log(newDict);
